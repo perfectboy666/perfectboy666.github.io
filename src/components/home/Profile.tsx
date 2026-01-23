b074912aa84addc0ba4f0621695a6d0688443486
tyshi <perfectboy666@outlook.com>
2026-01-24 00:07:04 +0800

    Update Profile.tsx

--- src/components/home/Profile.tsx
+++ src/components/home/Profile.tsx
@@ -350,10 +350,10 @@ export default function Profile({ author, social, features, researchInterests }:
                         </AnimatePresence>
                     </div>
                 </div>
-                <div> 
-                    <script type="text/javascript" id="clstr_globe" src="//clustrmaps.com/globe.js?d=FWZFisT0_5-j7dpwkppxFYz7ygttuBi3zATDSK_5f3Y"></script>
-                </div>
             )}
         </motion.div>
+        <div> 
+            <script type="text/javascript" id="clstr_globe" src="//clustrmaps.com/globe.js?d=FWZFisT0_5-j7dpwkppxFYz7ygttuBi3zATDSK_5f3Y"></script>
+        </div>
     );
 }
